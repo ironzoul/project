deaa1c7382712d17b697b7b628a6c5891a0a4783
ironzoul <66999232+ironzoul@users.noreply.github.com>
2022-05-18 15:11:34 +0530

    Add files via upload

--- pages/recruiter.js
+++ pages/recruiter.js
@@ -1,15 +1,148 @@
+import * as React from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { sanityClient,urlFor } from '../lib/sanity'
-import Link from 'next/link'
-import { Button
-} from '@mui/material'
-import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import { Button} from '@mui/material'
 import FormControl from '@mui/material/FormControl';
 import { styled } from '@mui/material/styles';
 import { grey, purple} from '@mui/material/colors';
+import PropTypes from 'prop-types';
+import SelectUnstyled, { selectUnstyledClasses } from '@mui/base/SelectUnstyled';
+import OptionUnstyled, { optionUnstyledClasses } from '@mui/base/OptionUnstyled';
+import PopperUnstyled from '@mui/base/PopperUnstyled';
 
+const blue = {
+  100: '#DAECFF',
+  200: '#99CCF3',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  900: '#003A75',
+};
+
+const StyledButton = styled('button')(
+  ({ theme }) => `
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  Height: auto;
+  width: 100%;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  border-radius: 0.75em;
+  margin-top: 0.5em;
+  padding: 10px;
+  text-align: left;
+  line-height: 1.5;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+
+  &:hover {
+    background: ${theme.palette.mode === 'dark' ? '' : grey[100]};
+    border-color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+  }
+
+  &.${selectUnstyledClasses.focusVisible} {
+    outline: 3px solid ${theme.palette.mode === 'dark' ? blue[600] : blue[100]};
+  }
+
+  &.${selectUnstyledClasses.expanded} {
+    &::after {
+      content: '▴';
+    }
+  }
+
+  &::after {
+    content: '▾';
+    float: right;
+  }
+  `,
+);
+
+const StyledListbox = styled('ul')(
+  ({ theme }) => `
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  padding: 5px;
+  margin: 10px 0;
+  align: center;
+  min-width: 320px;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  border-radius: 0.75em;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  overflow: auto;
+  outline: 0px;
+  `,
+);
+
+const StyledOption = styled(OptionUnstyled)(
+  ({ theme }) => `
+  list-style: none;
+  padding: 8px;
+  border-radius: 0.45em;
+  cursor: default;
+  align-items: center;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+
+  &.${optionUnstyledClasses.selected} {
+    background-color: ${theme.palette.mode === 'dark' ? blue[900] : blue[100]};
+    color: ${theme.palette.mode === 'dark' ? blue[100] : blue[900]};
+  }
+
+  &.${optionUnstyledClasses.highlighted} {
+    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  }
+
+  &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
+    background-color: ${theme.palette.mode === 'dark' ? blue[900] : blue[100]};
+    color: ${theme.palette.mode === 'dark' ? blue[100] : blue[900]};
+  }
+
+  &.${optionUnstyledClasses.disabled} {
+    color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+  }
+
+  &:hover:not(.${optionUnstyledClasses.disabled}) {
+    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  }
+  `,
+);
+
+const StyledPopper = styled(PopperUnstyled)`
+  z-index: 1;
+`;
+
+
+function CustomSelect(props) {
+  const components = {
+    Root: StyledButton,
+    Listbox: StyledListbox,
+    Popper: StyledPopper,
+    
+  };
+
+  return <SelectUnstyled {...props} components={components} />;
+}
+
+CustomSelect.propTypes = {
+  /**
+   * The components used for each slot inside the Select.
+   * Either a string to use a HTML element or a component.
+   * @default {}
+   */
+  components: PropTypes.shape({
+    Listbox: PropTypes.elementType,
+    Popper: PropTypes.func,
+    Root: PropTypes.elementType,
+  }),
+};
 
   
   const ColorButton = styled(Button)(({ theme }) => ({
@@ -20,7 +153,7 @@ import { grey, purple} from '@mui/material/colors';
     },
   }));
 
-const ColorInput = styled(TextField)(({ theme }) => ({
+const ColorInput = styled(Select)(({ theme }) => ({
   color: theme.palette.getContrastText(grey[50]),
   backgroundColor: grey[50],
   '&:hover': {
@@ -36,8 +169,15 @@ const jobsQuery = `*[_type == "job"]{
 
 
 
-export default function Recruiter({jobs}) {
+
+export default function Recruiter() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   
+  const [value, setValue] = React.useState(10);
   return (
     <>
     <div>
@@ -56,10 +196,17 @@ export default function Recruiter({jobs}) {
           <label >
             <span ><h3>Organization</h3></span><br />
             <FormControl fullWidth sx={{ m: 1 }}>
-            <ColorInput label="Organization"
-          multiline
-          color="primary"
-          maxRows={4}/>
+            <div>
+      <CustomSelect value={value} onChange={setValue}>
+        <StyledOption value={'Airtel'}>Airtel</StyledOption>
+        <StyledOption value={'Amazon'}>Amazon</StyledOption>
+        <StyledOption value={'Flipkart'}>Flipkart</StyledOption>
+        <StyledOption value={'Reliance Industries'}>Reliance Industries</StyledOption>
+        <StyledOption value={'JP Morgan'}>JP morgan</StyledOption>
+      </CustomSelect>
+
+     
+    </div>
         </FormControl>
             </label>
             <br />
@@ -68,10 +215,19 @@ export default function Recruiter({jobs}) {
             <span ><h3>Job Title</h3></span><br />
             <FormControl fullWidth sx={{ m: 1 }} background="white">
           
-            <ColorInput label="Job Title"
-          multiline
-          color="primary"
-          maxRows={4}/>
+            
+          <FormControl>
+          <div>
+      <CustomSelect value={value} onChange={setValue}>
+        <StyledOption value={'Software Engineer'}>Software Engineer</StyledOption>
+        <StyledOption value={'Web Developer'}>Web Developer</StyledOption>
+        <StyledOption value={'Database Admin'}>Database Admin</StyledOption>
+        <StyledOption value={'Technical Consultant'}>Technical Consultant</StyledOption>
+      </CustomSelect>
+
+     
+    </div>
+      </FormControl>
         </FormControl>
             
           </label>
